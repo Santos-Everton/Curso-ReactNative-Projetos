9f6f668b39deca5339429861ee55f829d433e065
Santos-Everton <everton.gomes@aluno.ufrb.edu.br>
2023-01-13 20:36:56 -0300

    Construindo API Express

---
+++ track-server/src/index.js
@@ -0,0 +1,28 @@
+require('./models/User');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = "mongodb+srv://admin:22021994@cluster0.vsbwgqd.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(mongoUri);
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo', err);
+});
+
+app.get('/', requireAuth, (req, res) => {
+    res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
+});
